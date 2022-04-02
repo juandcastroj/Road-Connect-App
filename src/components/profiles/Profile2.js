bea0b5024458836d7fd86967fde8f686dfb1ca70
aleja5313 <aleja_5313@hotmail.com>
2022-04-02 18:40:25 -0500

    locacion inicial

---
+++ src/components/profiles/Profile2.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Nav, Card } from "react-bootstrap";
+import {
+  Birthday,
+  CardProfile,
+  ContHomeRoad,
+  Rout,
+  RoutCont,
+} from "../../styles/styles";
+import Footer from "../Footer";
+import Naveg from "../Naveg";
+import profile2 from "../../images/Profile2.jpg";
+import profile from "../../images/futurama-leela.png";
+import birthday from "../../images/icons8-fecha-de-nacimiento-50.png";
+
+const Profile2 = () => {
+  return (
+    <div>
+      <Naveg />
+      <ContHomeRoad>
+        <Card className="text-center" width="40%">
+          <Card.Header>Mi Perfil</Card.Header>
+          <CardProfile>
+            <img src={profile2} width="50%" alt="epa"></img>
+            <hr></hr>
+            <img src={profile} width="4%" alt="epa"></img>
+            <h2>ANDREA MORENO</h2>
+            <Birthday src={birthday} width="3%" alt="epa"></Birthday>
+            <h4 style={{ display: "inline-block" }}>Fecha de Nacimiento</h4>
+            <p>27 Enero 2022</p>
+            <p>
+              Hijo de padres aficionados del ciclismo. Por tanto, esa pasión por
+              las montañas <br></br>y el deporte me fue transmitido desde bien
+              pequeño, y ahora recorro cada lugar por donde ellos cruzarón.
+            </p>
+            <Rout variant="warning"> Rutas Realizadas</Rout>
+            <Nav.Link href="/routes/3">Patios Sopo Bogotá</Nav.Link>
+            <Nav.Link href="/routes/14">Bogotá Embalse San Rafael Bogotá</Nav.Link>
+            <Nav.Link href="/routes/5">Bogotá Tenjo Bogotá</Nav.Link>
+            <hr></hr>
+            <RoutCont variant="warning">Contactar</RoutCont>
+          </CardProfile>
+          <Card.Footer className="text-muted">MTB y Ruta</Card.Footer>
+        </Card>
+      </ContHomeRoad>
+      <Footer />
+    </div>
+  );
+};
+
+export default Profile2;
--- src/components/profiles/Profile6.js
+++ src/components/profiles/Profile6.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Button, Card, Nav } from 'react-bootstrap'
+import {  Card, Nav } from 'react-bootstrap'
 import { Birthday, CardProfile, ContHomeRoad, Rout, RoutCont } from '../../styles/styles'
 import Footer from '../Footer'
 import Naveg from '../Naveg'
-import profile8 from '../../images/Profile8.jpg'
-import profile from '../../images/futurama-leela.png'
+import profile6 from '../../images/profile6.jpg'
+import profile from '../../images/futurama-fry-.png'
 import birthday from '../../images/icons8-fecha-de-nacimiento-50.png'
 
-const Profile8 = () => {
+const Profile6 = () => {
 
     return (
 
@@ -17,22 +17,22 @@ const Profile8 = () => {
                 <Card className="text-center" width='40%' >
                     <Card.Header>Mi Perfil</Card.Header>
                     <CardProfile>
-            <img src={profile8} width="50%"  alt="epa"></img>
+            <img src={profile6} width="50%"  alt="epa"></img>
             <hr></hr>
             <img src={profile} width="4%" alt="epa"></img>
-            <h2>MONICA MONTES</h2>
+            <h2>Monic</h2>
             <Birthday src={birthday} width="3%" alt="epa"></Birthday>
-            <h4  style={{display: 'inline-block' ,margin:'5px'}}>Fecha de Nacimiento</h4>
-            <p>04 Marzo 2022</p>
-            <p>Soy de Bogotá y recorro sus alrededores cada dia <br>
-            </br>exigiendome mas y conociendo
-                lugares y personas increibles.
+            <h4   style={{display: 'inline-block'}}>Fecha de Nacimiento</h4>
+            <p>11 Agosto 2022</p>
+            <p>
+              Siempre que quiero salir a rodar no encuentro con quien hacerlo, pero 
+              desde que estoy co esta app siempre hay alguien con quien salir a dar pedal. 
+              
             </p>
             <Rout variant="warning"> Rutas Realizadas</Rout>
-            <Nav.Link href="/routes/1">Bogota Alto de Patios</Nav.Link>
-            <Nav.Link href="/routes/0">Bogota La Vega Bogota</Nav.Link>
-            <Nav.Link href="/routes/4"> Bogota Embalse Sumapaz Bogota</Nav.Link>
-            <hr></hr>        
+            <Nav.Link href="/routes/11">Bogotá Tabio Bogotá</Nav.Link>
+            <Nav.Link href="/routes/4">Bogotá Tenjo Bogotá</Nav.Link>
+            <hr></hr>
             <RoutCont variant="warning">Contactar</RoutCont>
           </CardProfile>
                     <Card.Footer className="text-muted">MTB y Ruta</Card.Footer>
@@ -43,4 +43,4 @@ const Profile8 = () => {
     )
 }
 
-export default Profile8
+export default Profile6
